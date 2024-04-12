dfe67c2e58ccc4fe73d28589220000ef9bee2529
Isidro Valtueña <isidrovaltu04@gmail.com>
2024-04-12 19:06:24 -0300

    Se agrego una funcion que retorna el promedio de goles por partido del equipo

--- procesar_premios/__init__.py
+++ procesar_premios/__init__.py
@@ -18,3 +18,8 @@ def get_most_influent_player(dic):
     influence_list = [(name, (stats[0] * 1.5 + stats[1] * 1.25 + stats[2])) for name, stats in dic.items()]
     most_influent = max(influence_list, key = lambda x: x[1])
     return most_influent[0]
+
+def get_average_goals(goals):
+    """This function recieves the number of goals and returns the average per match value"""
+    average_goals = sum(goals) / 25
+    return average_goals
